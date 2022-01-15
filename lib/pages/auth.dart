5c7b2c0bfb09c91cb0b364803e588e6e74f90403
Anthony van Voorst <anthony_2v@hotmail.com>
2022-01-15 15:08:02 -0500

    Added background to login

--- lib/pages/auth.dart
+++ lib/pages/auth.dart
@@ -19,7 +19,15 @@ class _AuthPageState extends State<AuthPage> {
         title: Text('Login'),
       ),
       body: Container(
-        margin: EdgeInsets.all(10.0),
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/pexels-mehrad-vosoughi-3687999.jpg'),
+            fit: BoxFit.cover,
+            colorFilter: ColorFilter.mode(
+                Colors.black.withOpacity(0.6), BlendMode.dstATop),
+          ),
+        ),
+        padding: EdgeInsets.all(10.0),
         child: ListView(
           children: <Widget>[
             TextField(
