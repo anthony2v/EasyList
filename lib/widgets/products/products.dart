f0d9e81dfba510962039b6efb4bf9e0711bea661
Anthony van Voorst <anthony_2v@hotmail.com>
2022-01-15 16:37:49 -0500

    Created product card widget

--- lib/widgets/products/products.dart
+++ lib/widgets/products/products.dart
@@ -1,75 +1,22 @@
-import 'package:first_app/widgets/products/price_tag.dart';
+import 'package:first_app/widgets/products/product_card.dart';
 import 'package:flutter/material.dart';
 
 class Products extends StatelessWidget {
-  final List<Map<String, dynamic>> products;
-  final bool _isFavorite = false;
+  final List<Map<String, dynamic>> _products;
 
   // Constructor with a shortcut to automatically assign a variable to a local variable.
-  Products(this.products) {
+  Products(this._products) {
     print('[Products Widget Constructor]');
-  }
-
-  Widget _buildProductItem(BuildContext context, int index) {
-    return Card(
-      child: Column(
-        children: <Widget>[
-          Image.asset(products[index]['image']),
-          Padding(
-            padding: EdgeInsets.only(top: 10.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  products[index]['title'],
-                  style: TextStyle(
-                      fontSize: 26.0,
-                      fontWeight: FontWeight.w700,
-                      fontFamily: 'Oswald'),
-                ),
-                SizedBox(width: 8.0),
-                PriceTag(products[index]['price'].toString()),
-              ],
-            ),
-          ),
-          Container(
-            padding: EdgeInsets.symmetric(horizontal: 6.0, vertical: 2.5),
-            decoration: BoxDecoration(
-              border: Border.all(color: Colors.grey, width: 1.0),
-              borderRadius: BorderRadius.circular(4.0),
-            ),
-            child: Text('Union Square, San Francisco'),
-          ),
-          ButtonBar(
-            alignment: MainAxisAlignment.center,
-            children: <Widget>[
-              IconButton(
-                  color: Theme.of(context).colorScheme.secondaryVariant,
-                  icon: Icon(Icons.info),
-                  onPressed: () => Navigator.pushNamed<bool>(
-                      context, '/product/' + index.toString())),
-              IconButton(
-                  color: Colors.red,
-                  onPressed: () {
-                    print('Product favorited');
-                    // TODO turn into stateful widget and flip _isFavorite
-                  },
-                  icon: Icon(
-                      _isFavorite ? Icons.favorite : Icons.favorite_border))
-            ],
-          )
-        ],
-      ),
-    );
   }
 
   Widget _buildProductList() {
     print('[Products Widget] _buildProductList()');
     Widget productCards;
-    if (products.length > 0) {
+    if (_products.length > 0) {
       productCards = ListView.builder(
-        itemBuilder: _buildProductItem,
-        itemCount: products.length,
+        itemBuilder: (BuildContext context, index) =>
+            ProductCard(_products[index], index),
+        itemCount: _products.length,
       );
     } else {
       productCards = Center(child: Text("No products found. Please add some."));
