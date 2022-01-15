a788d5e1e56e754cd89c3888805c01e9c2b3386b
Anthony van Voorst <anthony_2v@hotmail.com>
2022-01-15 15:16:26 -0500

    Made text fields more visible

--- lib/pages/auth.dart
+++ lib/pages/auth.dart
@@ -24,46 +24,57 @@ class _AuthPageState extends State<AuthPage> {
             image: AssetImage('assets/pexels-mehrad-vosoughi-3687999.jpg'),
             fit: BoxFit.cover,
             colorFilter: ColorFilter.mode(
-                Colors.black.withOpacity(0.6), BlendMode.dstATop),
+                Colors.black.withOpacity(0.4), BlendMode.dstATop),
           ),
         ),
         padding: EdgeInsets.all(10.0),
-        child: ListView(
-          children: <Widget>[
-            TextField(
-              decoration: InputDecoration(labelText: 'E-Mail'),
-              keyboardType: TextInputType.emailAddress,
-              onChanged: (String value) {
-                setState(() {
-                  _emailValue = value;
-                });
-              },
+        child: Center(
+          child: SingleChildScrollView(
+            child: Column(
+              children: <Widget>[
+                TextField(
+                  decoration: InputDecoration(
+                      labelText: 'E-Mail',
+                      filled: true,
+                      fillColor: Colors.white),
+                  keyboardType: TextInputType.emailAddress,
+                  onChanged: (String value) {
+                    setState(() {
+                      _emailValue = value;
+                    });
+                  },
+                ),
+                SizedBox(height: 10.0),
+                TextField(
+                  decoration: InputDecoration(
+                      labelText: 'Password',
+                      filled: true,
+                      fillColor: Colors.white),
+                  obscureText: true,
+                  onChanged: (String value) {
+                    setState(() {
+                      _passwordValue = value;
+                    });
+                  },
+                ),
+                SwitchListTile(
+                  value: _acceptTerms,
+                  onChanged: (bool value) {
+                    setState(() {
+                      _acceptTerms = value;
+                    });
+                  },
+                  title: Text('Accept Terms'),
+                ),
+                ElevatedButton(
+                  child: Text('LOGIN'),
+                  onPressed: () {
+                    Navigator.pushReplacementNamed(context, '/products');
+                  },
+                ),
+              ],
             ),
-            TextField(
-              decoration: InputDecoration(labelText: 'Password'),
-              obscureText: true,
-              onChanged: (String value) {
-                setState(() {
-                  _passwordValue = value;
-                });
-              },
-            ),
-            SwitchListTile(
-              value: _acceptTerms,
-              onChanged: (bool value) {
-                setState(() {
-                  _acceptTerms = value;
-                });
-              },
-              title: Text('Accept Terms'),
-            ),
-            ElevatedButton(
-              child: Text('LOGIN'),
-              onPressed: () {
-                Navigator.pushReplacementNamed(context, '/products');
-              },
-            ),
-          ],
+          ),
         ),
       ),
     );
