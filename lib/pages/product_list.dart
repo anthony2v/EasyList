cef63f850e0e0223422909a2c84057f5874b1a9a
Anthony van Voorst <anthony_2v@hotmail.com>
2022-01-30 11:31:16 -0500

    Touched up My Products list

--- lib/pages/product_list.dart
+++ lib/pages/product_list.dart
@@ -12,25 +12,31 @@ class ProductListPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return ListView.builder(
         itemBuilder: (BuildContext context, int index) {
-          return ListTile(
-            leading: Padding(
-                padding: EdgeInsets.all(5.0),
-                child: Image.asset(_products[index]['image'])),
-            title: Text(_products[index]['title']),
-            trailing: IconButton(
-              icon: Icon(Icons.edit),
-              onPressed: () => Navigator.of(context).push(
-                MaterialPageRoute(
-                  builder: (BuildContext context) {
-                    return ProductEditPage(
-                      updateProduct: _updateProduct,
-                      product: _products[index],
-                      productIndex: index,
-                    );
-                  },
+          return Column(
+            children: [
+              ListTile(
+                leading: CircleAvatar(
+                    backgroundImage: AssetImage(_products[index]['image'])),
+                title: Text(_products[index]['title']),
+                subtitle:
+                    Text('\$${_products[index]['price'].toStringAsFixed(2)}'),
+                trailing: IconButton(
+                  icon: Icon(Icons.edit),
+                  onPressed: () => Navigator.of(context).push(
+                    MaterialPageRoute(
+                      builder: (BuildContext context) {
+                        return ProductEditPage(
+                          updateProduct: _updateProduct,
+                          product: _products[index],
+                          productIndex: index,
+                        );
+                      },
+                    ),
+                  ),
                 ),
               ),
-            ),
+              Divider(),
+            ],
           );
         },
         itemCount: _products.length);
