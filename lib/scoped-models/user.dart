0fcba91793fa11a0c02704a122c7f2bb34c97602
Anthony van Voorst <anthony_2v@hotmail.com>
2022-02-26 21:20:46 -0500

    Create user scoped model

---
+++ lib/scoped-models/user.dart
@@ -0,0 +1,12 @@
+import 'package:scoped_model/scoped_model.dart';
+
+import '../models/user.dart';
+
+class UserModel extends Model {
+  User _authenticatedUser;
+
+  void login(String email, String password) {
+    _authenticatedUser =
+        User(id: "f13jfi13g4g2g3fwe", email: email, password: password);
+  }
+}
